d6bd5f8e481de80d9ec3af539672aea7b857e4a9
Makiko <trimakichan@gmail.com>
2025-11-03 20:46:49 -0800

    creates one(Goal) to many relationship(Task) in the schema

--- app/models/goal.py
+++ app/models/goal.py
@@ -1,10 +1,14 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String
+from typing import TYPE_CHECKING
 from ..db import db
 
+if TYPE_CHECKING:
+    from .task import Task
 class Goal(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str] = mapped_column(String(255))
+    tasks: Mapped[list["Task"]] = relationship(back_populates="goal")
 
     def to_dict(self):
         return {
--- app/models/task.py
+++ app/models/task.py
@@ -1,14 +1,18 @@
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, ForeignKey
 from datetime import datetime
-from typing import Optional
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
+from typing import Optional, TYPE_CHECKING
 from ..db import db
 
+if TYPE_CHECKING:
+    from .goal import Goal
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]= mapped_column(String(255))
     description: Mapped[str] 
     completed_at: Mapped[Optional[datetime]] 
+    goal_id: Mapped[Optional[int]] = mapped_column(ForeignKey("goal.id"))
+    goal: Mapped[Optional["Goal"]] = relationship(back_populates="tasks")
 
     # Consider moving these methods into Base class after wave 3
     def to_dict(self):
