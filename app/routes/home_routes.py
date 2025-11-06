01928c32308cafcc399ebfe0e4919ee6a4251bda
Makiko <trimakichan@gmail.com>
2025-11-05 21:29:40 -0800

    fix: correct HTML syntax in home route welcome message

--- app/routes/home_routes.py
+++ app/routes/home_routes.py
@@ -5,4 +5,4 @@ bp = Blueprint("home_bp", __name__)
 
 @bp.get("/")
 def home():
-    return "<h1 classname> Welcome to Makiko's Task/Goals Search! </h1>"
+    return "<h1> Welcome to Makiko’s Task & Goal Search! </h1>"
